3141e878c69fe8cee42065077a3acab0b35b66d1
source-code888 <tafoyacabrera.antonio21@gmail.com>
2022-12-10 20:37:13 -0600

    Cambios

--- src/main/Aplicacion.java
+++ src/main/Aplicacion.java
@@ -5,6 +5,7 @@ import main.view.Login;
 public class Aplicacion {
 
     public static void main(String[] args) {
+        //Abrimos la interfaz del login
         Login login = new Login();
         login.setTitle("Iniciar sesion");
         login.setLocationRelativeTo(login);
